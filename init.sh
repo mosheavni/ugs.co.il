f025f5ad5c3cf2467106ff62d6f5499af2154ae2
Moshe Avni <mavni@netapp.com>
2024-04-10 01:09:27 +0300

    🌒 work in progress Wed Apr 10 01:09:26 2024

--- init.sh
+++ init.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo yum update
-sudo yum install -y docker git
+sudo yum install -y docker git wget
 service docker start
 sudo curl -L https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
